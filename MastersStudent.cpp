84a0d798966dd039656440b753d0737deb6bfe2c
LuuucyXuuu <xu6@cooper.edu>
2017-12-07 11:10:58 -0500

    Completed

--- MastersStudent.cpp
+++ MastersStudent.cpp
@@ -1,8 +1,11 @@
 #include "MastersStudent.h"
+#include <iostream>
+#include <iomanip>
 
 // Constructor Implementations
 MastersStudent::MastersStudent(Student ug, int msy):
-    Student(ug), _ms_grad_year(msy) {}
+    Student(ug), _ms_grad_year(msy) {
+	}
   
 MastersStudent::MastersStudent(std::string fn, std::string ln,
     int ugy, int msy, Major m):
@@ -10,16 +13,24 @@ MastersStudent::MastersStudent(std::string fn, std::string ln,
 
 // New methods specific to MastersStudent
 float MastersStudent::getMsGpa() {
-  // TODO Implement this 
-  return 0.0;
+  	float sum = 0;
+	int i;
+	for(i=0;i<_ms_grades.size();i++){
+		sum = _ms_grades[i] + sum;
+	}
+	return sum / _ms_grades.size();
 }
 
 void MastersStudent::addMsGrade(float grade) {
+	_ms_grades.push_back (grade);
+	return;
   // TODO Implement this 
 }
 
 // Override Student's printInfo to include new fields
 void MastersStudent::printInfo() {
   Student::printInfo();
+  std::cout << "MS " << majorString(_major) << ": " << _ms_grad_year << std::endl;
+  std::cout << "MS GPA: " << std::setprecision(2) << getMsGpa() << std::endl;
   // TODO Print additional MastersStudent fields
 }
